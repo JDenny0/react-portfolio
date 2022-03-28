f711e7d196685df6ffaafc80b8abd621591b3892
JDenny0 <denisjakubec@hotmail.com>
2022-03-28 21:41:04 +0200

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react"
+import logo from "../JD-logo.png";
+const Navbar = () => {
+  return (
+ <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+  <a className="navbar-brand" href="#"><img className="logo"src={logo}></img></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item active mx-5">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item mx-5 ">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item mx-5">
+        <a className="nav-link" href="#">Link</a>
+      </li> <li className="nav-item mx-5">
+        <a className="nav-link" href="#">Link</a>
+      </li> <li className="nav-item mx-5">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
